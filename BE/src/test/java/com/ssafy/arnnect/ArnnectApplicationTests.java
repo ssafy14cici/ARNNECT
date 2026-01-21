7a1ce70bfc40466cbb0496a2d3244ebf3cb384fb
Sujin31 <52381101+Sujin31@users.noreply.github.com>
2026-01-21 20:18:06 +0900

    FEAT: 백엔드 초기 구조만 추가

---
+++ BE/src/test/java/com/ssafy/arnnect/ArnnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.arnnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArnnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
