a317d00a1db2342cc44692a91dbb8cc889d3566e
이수진 <dltnwls301@gmail.com>
2026-01-30 14:41:05 +0900

    FEAT: 에러 레벨 수정 및 시큐리티 전역 허용 설정(임시)

--- BE/src/main/java/com/ssafy/arnnect/common/exception/GlobalExceptionHandler.java
+++ BE/src/main/java/com/ssafy/arnnect/common/exception/GlobalExceptionHandler.java
@@ -15,7 +15,7 @@ public class GlobalExceptionHandler {
             BusinessException e
     ) {
         ErrorCode errorCode = e.getErrorCode();
-        log.error("비즈니스 에러: code={}, 원본={}", e.getErrorCode(), e.getCause());
+        log.info("비즈니스 에러: code={}, 원본={}", e.getErrorCode(), e.getCause());
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(ApiResponse.fail(errorCode));
@@ -23,7 +23,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
-        log.error("전역 예외 발생: {}", e.getMessage(), e);
+        log.info("전역 예외 발생: {}", e.getMessage(), e);
         return ResponseEntity
                 .status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
                 .body(ApiResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR));
