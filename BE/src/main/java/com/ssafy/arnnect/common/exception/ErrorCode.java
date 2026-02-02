d5a5766794a90343f8fde2149bce9159ea88696a
최수원 <suwon1863@gmail.com>
2026-02-02 17:21:28 +0900

    FIX: merge 충돌 부분 수정

--- BE/src/main/java/com/ssafy/arnnect/common/exception/ErrorCode.java
+++ BE/src/main/java/com/ssafy/arnnect/common/exception/ErrorCode.java
@@ -4,23 +4,25 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 
-    // COMMON
+    /** COMMON **/
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_001", "잘못된 요청입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_002", "서버 오류가 발생했습니다."),
 
-    // AUTH
+    /** AUTH **/
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH_001", "인증에 실패했습니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH_002", "접근 권한이 없습니다."),
     USER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "AUTH_003", "잘못된 이메일 또는 비밀번호 입니다."),
 
-    // VALIDATION
+    /** VALIDATION **/
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "VALID_001", "요청 값이 올바르지 않습니다."),
 
-    // FILE
+    /** FILE **/
     FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "FILE_001", "파일을 찾을 수 없습니다."),
     FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "FILE_002", "파일 업로드에 실패했습니다."),
-    FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "FILE_003", "파일 삭제에 실패했습니다.");
+    FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "FILE_003", "파일 삭제에 실패했습니다."),
 
+    /** ARTWORK **/
+    ARTWORK_NOT_DOUND(HttpStatus.NOT_FOUND, "ARTWORK", "작품이 존재하지않습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
